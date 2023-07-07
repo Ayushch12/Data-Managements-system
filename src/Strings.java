e018056b75ece36232c69993553a2c7b0e798261
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-07 16:16:31 +0200

    Escape Sequences \, \"\", \t, \n

--- src/Strings.java
+++ src/Strings.java
@@ -1,20 +1,9 @@
 public class Strings {
     public static void main(String[] args) {
 
-        System.out.println("Madlibs !");
-
-        String username = "magic " ;
-
-        System.out.println("her name is ......and.........she is sister is very..... ");
-
-        String adjective = "Jay";
-        String verb = "fart";
-        String noun = "pogp sick";
-
-        System.out.println("her name is "+adjective+"and "+verb+" she is sister is very"+noun+ "");
+        System.out.println("ABC 123");
+        System.out.print("The \"Fish\" was blue");
 
     }
 }
 
-//Result: her name is ......and.........she is sister is very.....
-//her name is Jayand fart she is sister is verypogp sick
