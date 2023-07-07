9a54d98306fa637794ffe1edacce9eddd892cea8
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-07 16:08:53 +0200

    About String

---
+++ src/Strings.java
@@ -0,0 +1,20 @@
+public class Strings {
+    public static void main(String[] args) {
+
+        System.out.println("Madlibs !");
+
+        String username = "magic " ;
+
+        System.out.println("her name is ......and.........she is sister is very..... ");
+
+        String adjective = "Jay";
+        String verb = "fart";
+        String noun = "pogp sick";
+
+        System.out.println("her name is "+adjective+"and "+verb+" she is sister is very"+noun+ "");
+
+    }
+}
+
+//Result: her name is ......and.........she is sister is very.....
+//her name is Jayand fart she is sister is verypogp sick
