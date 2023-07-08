a700dfbcb9188c00d8580d900b6eca87cb0b40da
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 15:31:17 +0200

    WhileLoops -1

---
+++ src/WhileLoops.java
@@ -0,0 +1,12 @@
+public class WhileLoops {
+    public static void main(String[] args) {
+
+        int a = 0;
+
+        while (a < 10) {
+           System.out.println("a is less than 10" + a);
+           a++;
+
+        }
+    }
+}
