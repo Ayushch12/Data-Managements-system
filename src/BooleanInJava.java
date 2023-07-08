d9f2c78c9cff3aecbe12d425309afe0f39ac8a6c
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 11:39:43 +0200

    Boolean Java

---
+++ src/BooleanInJava.java
@@ -0,0 +1,14 @@
+public class BooleanInJava {
+
+    public static void main(String[] args) {
+
+
+        int a = 0;
+
+        //in boolean we can't write 0 or other variable except True or False.
+        boolean b = false;
+
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
