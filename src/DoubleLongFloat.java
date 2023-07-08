958df66a22582485550641bb608cec35c584ccea
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 11:31:27 +0200

    Long, Float and Double

---
+++ src/DoubleLongFloat.java
@@ -0,0 +1,19 @@
+public class DoubleLongFloat {
+
+    public static void main(String[] args) {
+
+        //int
+        int a = 1;
+        //long
+        long b = 222222222L;
+        //float
+        float c = 2.3f;
+        //double
+        double d = 533333333332222245678976545678987654567898.7654;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
--- src/Integers.java
+++ src/Integers.java
@@ -2,6 +2,7 @@ public class Integers {
 
     public static void main(String[] args) {
 
+        //This all are integers
         int a = 5;
         int b = 9;
         int c = -700;
