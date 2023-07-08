efe11d48b7ab98a33efbd44509279e44838475b4
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 15:02:28 +0200

    Use Comparison operation-1

---
+++ src/ComparisonOperatorsJava.java
@@ -0,0 +1,12 @@
+public class ComparisonOperatorsJava {
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 4;
+
+        boolean c = (a==b);
+
+        System.out.println(c);
+
+    }
+}
