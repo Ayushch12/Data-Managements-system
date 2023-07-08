af3e20c01fe85fc0a893b173a7305f42ca1ea156
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 11:59:29 +0200

    If statement in Boolean-2

--- src/BooleanInJava.java
+++ src/BooleanInJava.java
@@ -2,14 +2,34 @@ public class BooleanInJava {
 
     public static void main(String[] args) {
 
+        boolean passedDoor = true;
+        boolean missedDoor = false;
+        boolean passedAllDoors = false;
+        int doorCount = 0;
 
-if(true) {
-    System.out.println("We are in paris");
+
+if(passedDoor) {
+    System.out.println("We passes the first door!");
+    doorCount = doorCount + 1;
 }
-
-
-        if(false) {
-            System.out.println("We are in paris");
+        if(passedDoor) {
+            System.out.println("We passes the second door!");
+            doorCount = doorCount + 1;
         }
+        if(missedDoor) {
+            System.out.println("We passes the third door!");
+            doorCount = doorCount + 1;
+
+            if(doorCount == 3){
+              passedAllDoors = true;
+            }
+
+            if (passedAllDoors){
+                System.out.println("Congratualions you won program!");
+            }
+
+        }
+
     }
 }
+
