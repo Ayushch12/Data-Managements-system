c3d8a7bd84ca14607b0ff19f42eda198bf120a81
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 11:43:43 +0200

    If statement in Boolean

--- src/BooleanInJava.java
+++ src/BooleanInJava.java
@@ -3,12 +3,8 @@ public class BooleanInJava {
     public static void main(String[] args) {
 
 
-        int a = 0;
-
-        //in boolean we can't write 0 or other variable except True or False.
-        boolean b = false;
-
-        System.out.println(a);
-        System.out.println(b);
+if(true) {
+    System.out.println("We are in paris");
+}
     }
 }
