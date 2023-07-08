1446002e33492ba71a0218bd810fcbc2450e3984
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-08 11:23:52 +0200

    Int Variable

---
+++ src/Integers.java
@@ -0,0 +1,13 @@
+public class Integers {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+        int b = 9;
+        int c = -700;
+        int d = 500;
+
+        System.out.println(a*b);
+    }
+
+}
