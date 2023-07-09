e88079fe66d6a10ac43a83b9db4f61a25beb0ed7
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-09 14:38:31 +0200

    For loop

---
+++ src/Forloops.java
@@ -0,0 +1,24 @@
+public class Forloops {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println("I loved dogs!");
+        }
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+       for ( int i = 0; i < 20 ; i++){
+           System.out.println(i);
+       }
+
+       int[] grades = {98, 100, 83, 90, 93};
+
+       for (int i = 0; i < grades.length; i++){
+           System.out.println(grades[i]);
+       }
+
+    }
+}
