5d4a64598f3f08b2d5c9604695d2bb018522ba45
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-09 14:16:07 +0200

    Java Arrays

---
+++ src/ZombieKiller.java
@@ -0,0 +1,16 @@
+public class ZombieKiller {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Zombie killer program !! ");
+
+        String backpack [] = {"Shotgun", "Assault", "Sniper"};
+        String zombies [] = {"Close-range", "Mid-range"};
+
+        System.out.println("Backpack items !!");
+        System.out.println(backpack[0]);
+        System.out.println(backpack[1]);
+        System.out.println(backpack[2]);
+
+
+
+    }
+}
