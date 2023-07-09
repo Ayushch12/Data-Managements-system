f8149acf25b6facf8d9c705cc1502030a47a37db
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-09 16:02:51 +0200

    Logic Operators -3

--- src/Logic.java
+++ src/Logic.java
@@ -11,6 +11,9 @@ int b = 4;
 
         boolean c = !((a < b) && true) ;
 
+        if (a < b && isSunny ()){
+            System.out.println("Its a great day");
+        }
         System.out.println(c);
     }
 }
