b33cd33f8dea18c8dc4dbac6803bb2e84d8ca88d
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-09 16:10:58 +0200

    Initilization, Declaration and Assignment

---
+++ src/ExtraCheese.java
@@ -0,0 +1,14 @@
+public class ExtraCheese {
+    public static void main(String[] args) {
+
+        String pizza1 = "Pineapple";  // initialization
+        String pizza2 = "Orange";   // declaration
+        pizza2 = "bbq"; // assignment
+
+
+System.out.println(pizza1);
+        System.out.println(pizza2);
+    }
+
+
+}
--- src/Logic.java
+++ src/Logic.java
@@ -11,9 +11,7 @@ int b = 4;
 
         boolean c = !((a < b) && true) ;
 
-        if (a < b && isSunny ()){
-            System.out.println("Its a great day");
-        }
+
         System.out.println(c);
     }
 }
