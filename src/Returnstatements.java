e1aadc5acc60b9f5d57db851747edc1bf66e450a
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 22:13:59 +0200

    Return Statements -2

--- src/Returnstatements.java
+++ src/Returnstatements.java
@@ -2,14 +2,15 @@ public class Returnstatements {
 
     public static  void main (String[] args){
   printAMessage();
-  add(5, 4);
+  int sum =add(5, 4);
+  System.out.println(sum);
 
     }
     public static void printAMessage(){
         System.out.println("This is our first method!");
     }
-    public static void add(int a, int b){
-        System.out.println(a+b);
+    public static int add(int a, int b){
+        return a+b;
     }
 
 }
