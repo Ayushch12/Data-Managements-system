855e825e2907ba6a190fb5dc33d7a10ad57b5118
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 19:34:58 +0200

    String Methods -1

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,6 +1,6 @@
 public class NestedLoops {
 
-    public static void main (String [] args ) {
+    public static void main (String[] args ) {
 
         for(int i = 0; i < 5; i++){
             System.out.println("It is so freaking cold outside!");
---
+++ src/Stringmethods.java
@@ -0,0 +1,9 @@
+public class Stringmethods {
+public static void main (String [] args){
+
+    String name = "Billy Bob Joe";
+    System.out.println("Name: " + name);
+
+}
+
+}
