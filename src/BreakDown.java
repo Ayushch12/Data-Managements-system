fd1e7642c0f48fb35839f3e1cc2314971863285a
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 12:26:05 +0200

    BreakDown-1

---
+++ src/BreakDown.java
@@ -0,0 +1,11 @@
+public class BreakDown {
+
+    public static void main (String[] args) {
+
+        while (true) {
+            System.out.println("Hi!");
+            break;
+        }
+
+    }
+}
