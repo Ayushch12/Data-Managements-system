1c760ee7e98df6683346177df97d2c8e1b1bf94e
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 21:12:55 +0200

    Return Statements -1

---
+++ src/Returnstatements.java
@@ -0,0 +1,15 @@
+public class Returnstatements {
+
+    public static  void main (String[] args){
+  printAMessage();
+  add(5, 4);
+
+    }
+    public static void printAMessage(){
+        System.out.println("This is our first method!");
+    }
+    public static void add(int a, int b){
+        System.out.println(a+b);
+    }
+
+}
