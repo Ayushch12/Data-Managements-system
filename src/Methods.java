9d27808405498637da6c1632b96d8d623c2bb0d6
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 14:54:28 +0200

    Methods -3

--- src/Methods.java
+++ src/Methods.java
@@ -1,17 +1,22 @@
 public class Methods {
-    public static void main (String[] args){
-
-        multiply(5,10);
-        multiply(2,100);
-        multiply(3,20);
-
-
+    public static void main(String[] args) {
+        welcome();
+        multiply(5, 10);
+        multiply(2, 100);
+        multiply(3, 20);
+        divide(100, 50);
+        divide(200, 100);
     }
 
-    public static void multiply(int a, int b){
-        System.out.println(a*b);
-
-
+    public static void welcome() {
+        System.out.println("Welcome to our calculator!");
+    }
+    public static void multiply(int a, int b) {
+        System.out.println(a * b);
+    }
+    public static void divide (int a, int b) {
+        System.out.println(a % b);
     }
 
 }
+
