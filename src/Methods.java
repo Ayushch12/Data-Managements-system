e1dcf5de4c579f79c2abfab618cec40f10afd2b7
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 14:32:43 +0200

    Methods -2

--- src/Methods.java
+++ src/Methods.java
@@ -2,19 +2,14 @@ public class Methods {
     public static void main (String[] args){
 
         multiply(5,10);
+        multiply(2,100);
+        multiply(3,20);
 
-        int c = 5;
-        int d = 10;
-        System.out.println(c*d);
-
-
-        int e = 2;
-        int f = 3;
-        System.out.println(e*f);
 
     }
 
     public static void multiply(int a, int b){
+        System.out.println(a*b);
 
 
     }
