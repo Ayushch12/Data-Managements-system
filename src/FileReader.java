2f7a99fba6f1a074b9490890b9bb1e4bc9e94e87
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 23:51:20 +0200

    Read Text

---
+++ src/FileReader.java
@@ -0,0 +1,14 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class FileReader {
+
+    public static void main (String[] args){
+
+        File file = new File("User/ayush ") ;
+        Scanner scan = new Scanner(file);
+    }
+
+
+}
+
