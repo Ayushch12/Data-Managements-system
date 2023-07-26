32706cced46d9592fcfa05620dc44b68fa17e9c2
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 11:49:08 +0200

    Nested Loop-1

---
+++ src/NestedLoops.java
@@ -0,0 +1,14 @@
+public class NestedLoops {
+
+    public static void main (String [] args ) {
+
+        for(int i = 0; i < 5; i++){
+            System.out.println("It is so freaking cold outside!");
+        }
+        String[] colors = {"Red", "Blue", "Green"};
+
+        for (int i = 0; i<3; i++){
+            System.out.println(colors [i]);
+        }
+    }
+}
