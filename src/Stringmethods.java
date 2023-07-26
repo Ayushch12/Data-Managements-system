673fc3b5e03b60a0ff170729e4f14a3050ce4e5e
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 19:41:57 +0200

    String Methods -2, To make a lowercase, charAT

--- src/Stringmethods.java
+++ src/Stringmethods.java
@@ -7,6 +7,12 @@ public static void main (String [] args){
     // If we want to make a upperCase
     System.out.println("Uppercase: " + name.toUpperCase());
 
+    //Lower case
+    System.out.println("Lowercase: " + name.toLowerCase());
+
+
+    //ChatAt case
+    System.out.println("First char: " + name.charAt(2));
 }
 
 }
