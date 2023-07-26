3b8b619394f62dbf3c2547ded5b895418b18e5f7
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-26 19:38:37 +0200

    String Methods -2, To make a upperCase

--- src/Stringmethods.java
+++ src/Stringmethods.java
@@ -4,6 +4,9 @@ public static void main (String [] args){
     String name = "Billy Bob Joe";
     System.out.println("Name: " + name);
 
+    // If we want to make a upperCase
+    System.out.println("Uppercase: " + name.toUpperCase());
+
 }
 
 }
