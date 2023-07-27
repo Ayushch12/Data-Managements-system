dc2da2295a597827c258ce58ffba60a8711d6a1e
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-27 11:02:37 +0200

    Switch Statement -1

---
+++ src/SwitchStatement.java
@@ -0,0 +1,36 @@
+public class SwitchStatement {
+
+    public static void main(String[] args ){
+
+        int day = 3;
+
+        if (day == 1){
+            System.out.println("Sunday");
+        } else if (day == 2){
+            System.out.println("Monday");
+        }
+        else if (day == 3){
+            System.out.println("Tuesday");
+        }
+        else if (day == 4){
+            System.out.println("Wednesday");
+        }
+        else if (day == 5){
+            System.out.println("Thursday");
+        }
+        else if (day == 6){
+            System.out.println("Friday");
+        }
+
+//SWITCH STATEMENT : In Java, the switch statement is a control flow statement that allows
+// you to test the value of a variable or an expression against a list of possible constant values.
+// It provides a concise way to handle multiple conditional branches based on the value of a single
+// variable or expression.
+
+
+
+
+    }
+
+
+}
