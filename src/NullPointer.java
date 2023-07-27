6ff241fc51b83fb63f27fc0546349aa42d4279fd
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-27 14:31:45 +0200

    Null Pointer -2

--- src/NullPointer.java
+++ src/NullPointer.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
+
 public class NullPointer {
     public static void main (String [] args){
 
-        String a = null;
-        System.out.println(a.length());
+     System.out.println("enter a number");
+        Scanner scan = new Scanner (System.in);
+        System.out.println(scan.nextInt());
 
     }
 }
