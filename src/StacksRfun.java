c6888a39d418ee97bb66db1cf7bb964dce3b28c6
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-28 15:05:17 +0200

    Stack Java -1

---
+++ src/StacksRfun.java
@@ -0,0 +1,15 @@
+import java.util.Stack;
+
+public class StacksRfun {
+
+    public static void main (String[] args ){
+
+        Stack <String> games = new Stack <String> () ;
+
+        games.add("Call of Duty");
+        games.add("GTA");
+        games.add("Supper monkey ball");
+
+System.out.println(games);
+    }
+}
