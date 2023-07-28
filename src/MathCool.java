a0e6a98d6b27e4fcffb4d87976c4531a0b56376c
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-28 14:53:16 +0200

    Math Library -3 (Absolute value, Asin Value, Atan)

--- src/MathCool.java
+++ src/MathCool.java
@@ -3,8 +3,11 @@ public class MathCool {
     public static void main (String [] args) {
 
       // System.out.println(Math.E);
-        System.out.println(Math.PI);
-
+        //System.out.println(Math.PI); PIE
+        //System.out.println(Math.abs(-12)); ABSOLUTE VALUE
+       // System.out.println(Math.acos(.54));
+        // System.out.println(Math.asin(1)); ASIN VALUE
+      //  System.out.println(Math.atan2(6, 3)); ATAN
     }
 }
 
