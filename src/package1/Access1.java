57219404b68b1485537b9ae323e6b1a502d54bcf
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-28 14:38:58 +0200

    Access Modifiers -3 (Protected)

--- src/package1/Access1.java
+++ src/package1/Access1.java
@@ -4,7 +4,13 @@ public class Access1 {
 
 
     //default, public, private and protected
-   public  int hours = 3;
-   public  int minutes = 47;
+    protected int hours = 3;
+    protected int minutes = 47;
+
+      protected int getHours(){
+     return hours;
+
+    }
+
 
 }
