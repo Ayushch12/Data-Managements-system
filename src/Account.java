654aadd8a94c2eb75d7e60dbaa795d91893dae1c
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-29 16:28:01 +0200

    Getters and Setters -1

--- src/Account.java
+++ src/Account.java
@@ -10,10 +10,24 @@ public class Account {   // Class Declaration:
         a.age = 34;             // Setting Instance Variables:
         a.name = "Pablo";       // Setting Instance Variables:
 
-        System.out.println(a.age);    // Printing Instance Variables:
-        System.out.println(a.name);  //Printing Instance Variables:
+a.printDetails();
 
+    }
+    public void printDetails() {
+
+        System.out.println(name + ", " + age);
 
     }
 
+
+
 }
+// Getters:
+//A getter method is used to retrieve the value of an instance variable from an object.
+// It provides read-only access to the private fields of the class.
+
+
+//Setters:
+//A setter method is used to modify or update the value of an instance variable in an object.
+// It provides write-only access to the private fields of the class.
+
