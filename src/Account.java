9c5353c3a7101c0c4866e5a4c4d613c2b8f87bb4
Ayush chalisee <ayushchalise12@gmail.com>
2023-07-29 16:20:38 +0200

    Getters and Setters

---
+++ src/Account.java
@@ -0,0 +1,19 @@
+public class Account {   // Class Declaration:
+
+
+    String name ;   // Instance Variables:
+    int age;        // Instance Variables:
+    public static void main (String[] args){      // Main Method:
+
+        Account a = new Account ();    // Object Creation:
+
+        a.age = 34;             // Setting Instance Variables:
+        a.name = "Pablo";       // Setting Instance Variables:
+
+        System.out.println(a.age);    // Printing Instance Variables:
+        System.out.println(a.name);  //Printing Instance Variables:
+
+
+    }
+
+}
