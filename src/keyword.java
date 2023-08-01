0cbef38d13c167f4221b32256e4389775ac560fb
Ayush chalisee <ayushchalise12@gmail.com>
2023-08-01 22:04:34 +0200

    TRY / CATCH / FINALLY STATEMENT - 2

--- src/keyword.java
+++ src/keyword.java
@@ -4,12 +4,17 @@ public class keyword {
     public static  void main (String [] args){
 
         Scanner scan = new Scanner(System.in);
-        System.out.println(scan.next());
+
         try {
+
+            System.out.println(scan.next());
 
         } catch (Exception e){
 
+System.out.println(e);
+
         }finally {
+            scan.close();
 
         }
 
