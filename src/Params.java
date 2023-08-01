596fc1997bc9ef170958ce124a8786147ec13180
Ayush chalisee <ayushchalise12@gmail.com>
2023-08-01 22:11:38 +0200

    PARAMS

---
+++ src/Params.java
@@ -0,0 +1,10 @@
+public class Params {
+
+    public static void main (String[] args){
+      saySomething("Dog");
+    }
+
+    public static void saySomething(String s){
+        System.out.println(s);
+    }
+}
